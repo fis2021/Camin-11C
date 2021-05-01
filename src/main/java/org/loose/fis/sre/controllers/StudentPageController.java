a17650c8d79096465a8d46185cdb7675238ef38b
Iozsa Andrei <andrei.iozsa@student.upt.ro>
2021-05-02 02:10:01 +0300

    I made the student room page and button

--- src/main/java/org/loose/fis/sre/controllers/StudentPageController.java
+++ src/main/java/org/loose/fis/sre/controllers/StudentPageController.java
@@ -1,8 +1,23 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class StudentPageController {
-    public static String username;
-    public static void setClientUsername(String username){
-        StudentPageController.username = username;
+    @FXML
+    private Button roomProfileButton;
+
+    public void handleRoomProfileAction() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("roomProfile.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) roomProfileButton.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }

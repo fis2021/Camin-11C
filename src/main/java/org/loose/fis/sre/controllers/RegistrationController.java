c8dfa713935b3a7d2dbe662958a0a7ae49acb1ca
Iozsa Andrei <andrei.iozsa@student.upt.ro>
2021-05-01 23:23:58 +0300

    Auto stash before checking out "HEAD"

--- src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
+++ src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
@@ -1,11 +1,22 @@
 package org.loose.fis.sre.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+<<<<<<< Updated upstream
 import org.loose.fis.sre.services.UserService;
+=======
+import javafx.stage.Stage;
+import org.loose.fis.sre.exceptions.*;
+import org.loose.fis.sre.services.UserService;
+
+import java.io.IOException;
+>>>>>>> Stashed changes
 
 public class RegistrationController {
 
@@ -19,13 +30,21 @@ public class RegistrationController {
     private ChoiceBox role;
 
     @FXML
-    public void initialize() {
-        role.getItems().addAll("Student", "Admin");
+    public void handleRegisterAction() {
+        UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+        registrationMessage.setText("Account created successfully!");
     }
 
     @FXML
-    public void handleRegisterAction() {
-            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
-            registrationMessage.setText("Account created successfully!");
+    public void handleLoginAction(){
+//        try {
+//            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("login.fxml"));
+//            Parent root = loader.load();
+//            Stage stage = (Stage) loginButton.getScene().getWindow();
+//            stage.setScene(new Scene(root));
+//            stage.show();
+//        } catch (IOException e) {
+//            System.out.println(e.getCause());
+//        }
     }
 }

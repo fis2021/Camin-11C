96535c30c52e937001d357cf708e514ebb891505
Iozsa Andrei <andrei.iozsa@student.upt.ro>
2021-05-01 23:30:48 +0300

    Fixed some errors

--- src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
+++ src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
@@ -4,19 +4,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
-<<<<<<< Updated upstream
-import org.loose.fis.sre.services.UserService;
-=======
 import javafx.stage.Stage;
 import org.loose.fis.sre.exceptions.*;
-import org.loose.fis.sre.services.UserService;
+import org.loose.fis.sre.model.User;
+import org.loose.fis.sre.services.*;
 
 import java.io.IOException;
->>>>>>> Stashed changes
 
 public class RegistrationController {
 
@@ -28,6 +26,13 @@ public class RegistrationController {
     private TextField usernameField;
     @FXML
     private ChoiceBox role;
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Student", "Admin");
+    }
 
     @FXML
     public void handleRegisterAction() {
@@ -37,14 +42,14 @@ public class RegistrationController {
 
     @FXML
     public void handleLoginAction(){
-//        try {
-//            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("login.fxml"));
-//            Parent root = loader.load();
-//            Stage stage = (Stage) loginButton.getScene().getWindow();
-//            stage.setScene(new Scene(root));
-//            stage.show();
-//        } catch (IOException e) {
-//            System.out.println(e.getCause());
-//        }
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("login.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getCause());
+        }
     }
 }

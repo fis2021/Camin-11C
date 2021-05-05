1e2b210d013909d54c6047ac00398884425c6205
Deea <hanes.andreea@yahoo.com>
2021-05-05 15:02:08 +0300

    CM-82 GoBack button is created

--- src/main/java/org/loose/fis/sre/controllers/LaundryAppointmentController.java
+++ src/main/java/org/loose/fis/sre/controllers/LaundryAppointmentController.java
@@ -1,6 +1,14 @@
 package org.loose.fis.sre.controllers;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LaundryAppointmentController {
 
     @FXML
@@ -9,6 +17,18 @@ public class LaundryAppointmentController {
     private TextField dayField;
     @FXML
     private TextField hourField;
+    @FXML
+    private Button backToStudentHomePageButton;
+    public void handleBackToStudentHomePageAction() {
+        try{
+            Parent root= FXMLLoader.load(getClass().getClassLoader().getResource("studentPage.fxml"));
+            Stage stage = (Stage) (backToStudentHomePageButton.getScene().getWindow());
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+    }
 
     @FXML
     public void handleLaundryAppointmentAction(){

5d9ec694f714ac3e642615141d50049cb9f34ccf
Iozsa Andrei <andrei.iozsa@student.upt.ro>
2021-05-05 23:32:44 +0300

    CM-45 I created the table and it works

--- src/main/java/org/loose/fis/sre/controllers/PaymentsSituationController.java
+++ src/main/java/org/loose/fis/sre/controllers/PaymentsSituationController.java
@@ -1,14 +1,35 @@
 package org.loose.fis.sre.controllers;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import org.loose.fis.sre.model.Laundry;
+import org.loose.fis.sre.model.Room;
+import org.loose.fis.sre.model.User;
+import org.loose.fis.sre.services.LaundryService;
+import org.loose.fis.sre.services.RoomService;
+import org.loose.fis.sre.services.UserService;
 
 import java.io.IOException;
-public class PaymentsSituationController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
+public class PaymentsSituationController{
+    @FXML
+    private TableView paymentsTable;
+    @FXML
+    private TableColumn<User, String> nameCol;
+    @FXML
+    private TableColumn<Room, Integer> roomCol;
     @FXML
     private Button backToStudentHomePageButton;
 
--- src/main/java/org/loose/fis/sre/controllers/StudentsEvidenceController.java
+++ src/main/java/org/loose/fis/sre/controllers/StudentsEvidenceController.java
@@ -1,17 +1,36 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import org.loose.fis.sre.model.Evidence;
+import org.loose.fis.sre.model.Room;
+import org.loose.fis.sre.model.User;
 import org.loose.fis.sre.model.window;
+import org.loose.fis.sre.services.RoomService;
+import org.loose.fis.sre.services.UserService;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class StudentsEvidenceController {
-
+public class StudentsEvidenceController implements Initializable {
+    @FXML
+    private TableView<Evidence> evidenceTable;
+    @FXML
+    private TableColumn<Evidence, String> nameCol;
+    @FXML
+    private TableColumn<Evidence, Integer> roomCol;
     @FXML
     private Button backToAdminHomePageButton;
 
@@ -27,4 +46,25 @@ public class StudentsEvidenceController {
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ArrayList<User> users = UserService.getUsers();
+        ArrayList<Room> rooms = RoomService.getRooms();
+        ArrayList<Evidence> evidenceList = new ArrayList<>();
+
+        for(User user: UserService.getUserRepository().find()){
+            for(Room room: RoomService.getRoomRepository().find()){
+                if(user.getUsername().equals(room.getStudent1()) ||
+                        user.getUsername().equals(room.getStudent1())){
+                    evidenceList.add(new Evidence(user.getUsername(),room.getNrRoom()));
+                }
+            }
+        }
+
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        roomCol.setCellValueFactory(new PropertyValueFactory<>("room"));
+        ObservableList<Evidence> observableList = FXCollections.observableArrayList(evidenceList);
+        evidenceTable.setItems(observableList);
+        System.out.println("Showing the students");
+    }
 }

4ea7e946f02a1ba22961cdfcf64a5247341292b5
Deea <hanes.andreea@yahoo.com>
2021-05-13 20:33:09 +0300

    CM-102 Created tests for Laundry Appointment functionality

--- src/main/java/org/loose/fis/sre/controllers/LaundryAppointmentController.java
+++ src/main/java/org/loose/fis/sre/controllers/LaundryAppointmentController.java
@@ -3,6 +3,7 @@ package org.loose.fis.sre.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.loose.fis.sre.exceptions.IncorrectLaundryAppointmentException;
 import org.loose.fis.sre.model.window;
 import org.loose.fis.sre.services.LaundryService;
 
@@ -21,7 +22,7 @@ public class LaundryAppointmentController {
     }
 
     @FXML
-    public void handleLaundryAppointmentAction(){
+    public void handleLaundryAppointmentAction() throws IncorrectLaundryAppointmentException {
         LaundryService.addLaundry(Integer.parseInt(roomField.getText()),
                 dayField.getText(),
                 Integer.parseInt(hourField.getText()));

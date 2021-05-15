9789eec0954810954a980d2da153e4d804c19c28
Iozsa Andrei <andrei.iozsa@student.upt.ro>
2021-05-15 22:26:27 +0300

    CM-114 Laundry Schedule was tested with TestFX

--- src/main/test/java/org/loose/fis/sre/controllers/LaundryScheduleControllerTest.java
+++ src/main/test/java/org/loose/fis/sre/controllers/LaundryScheduleControllerTest.java
@@ -10,69 +10,63 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.loose.fis.sre.exceptions.*;
 import org.loose.fis.sre.services.FileSystemService;
+import org.loose.fis.sre.services.LaundryService;
+import org.loose.fis.sre.services.RoomService;
 import org.loose.fis.sre.services.UserService;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
+import static org.testfx.assertions.api.Assertions.assertThat;
 
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.testfx.assertions.api.Assertions.assertThat;
 
 @ExtendWith(ApplicationExtension.class)
-public class RegistrationControllerTest {
-
+public class LaundryScheduleControllerTest {
     public static final String USERNAME = "password";
     public static final String PASSWORD = "username";
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         UserService.closeDatabase();
     }
 
     @BeforeEach
-    void setUP() throws IOException {
+    void setUP() throws IOException, UsernameAlreadyExistsException, IncorrectLaundryAppointmentException {
         FileSystemService.APPLICATION_FOLDER = ".test-registration";
         FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
         UserService.initDatabase();
+        LaundryService.initDatabase();
+        UserService.addUser(USERNAME, PASSWORD, "Student");
+        LaundryService.addLaundry(130,"Monday",12);
+        LaundryService.addLaundry(125,"Friday",15);
     }
 
     @Start
     void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         primaryStage.setTitle("Camin 11C");
-        primaryStage.setScene(new Scene(root, 400, 475));
+        primaryStage.setScene(new Scene(root, 600, 475));
         primaryStage.show();
     }
 
     @Test
-    void testRegistration(FxRobot robot) {
+    void testLaundryScheduleController(FxRobot robot){
         robot.clickOn("#username");
         robot.write(USERNAME);
         robot.clickOn("#password");
         robot.write(PASSWORD);
         robot.clickOn("#role");
         robot.type(KeyCode.ENTER);
-        robot.clickOn("#registerButton");
-        assertThat(robot.lookup("#registrationMessage").queryText()).hasText("Account created successfully!");
-        assertThat(UserService.getUsers()).size().isEqualTo(1);
+        robot.clickOn("#loginButton");
 
-        robot.clickOn("#username");
-        robot.write("1");
-
-        robot.clickOn("#registerButton");
-        assertThat(robot.lookup("#registrationMessage").queryText()).hasText("Account created successfully!");
-        assertThat(UserService.getUsers()).size().isEqualTo(2);
-
-        robot.clickOn("#registerButton");
-        assertThat(robot.lookup("#registrationMessage").queryText()).hasText(
-                String.format("Sorry the username is already taken"));
-
-        robot.clickOn("#redirectLoginButton");
-        assertThat(robot.lookup("#loginButton")).isNotNull();
-
+        robot.clickOn("#laundryScheduleButton");
+        assertThat(robot.lookup("#laundryScheduleButton")).isNotNull();
+        robot.clickOn("#backToStudentHomePageButton");
+        assertThat(robot.lookup("#backToStudentHomePageButton")).isNotNull();
 
     }
 }
